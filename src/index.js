23a669d3fb459c227b7b309857734722b3457245
Duong Nguyen <duongf5vcu@gmail.com>
2021-04-26 01:25:37 +0700

    edit packagejson3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 
 // ========== Require Framwork =============\\
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const hdbs = require('express-handlebars');
 const path = require('path');
 const methodOverride = require('method-override');
